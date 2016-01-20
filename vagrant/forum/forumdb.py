e117c9d0cef6e5aa49e9297111055d898cff00e7
SeekingMonkey <cliftonlboyd@gmail.com>
2016-01-19 20:07:08 -0500

    SQL server working

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,10 +2,16 @@
 # Database access functions for the web forum.
 # 
 
-import time
+import time, psycopg2
 
 ## Database connection
 DB = []
+try:
+    conn = psycopg2.connect("dbname=forum")
+except:
+    print "I am unable to connect to the database"
+c = conn.cursor()
+
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,6 +22,8 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
+    c.execute("""SELECT content, time, id FROM posts ORDER BY time DESC""")
+    
     posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
     posts.sort(key=lambda row: row['time'], reverse=True)
     return posts
@@ -27,5 +35,6 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    c.execute("""INSERT INTO posts VALUES ('%s')""") #% (content)
+    #t = time.strftime('%c', time.localtime())
+    #DB.append((t, content))
