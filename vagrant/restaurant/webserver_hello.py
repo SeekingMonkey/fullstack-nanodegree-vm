6b5e036595af06ff2d79fde2c2dfcd9454b42508
SeekingMonkey <cliftonlboyd@gmail.com>
2016-02-07 00:18:57 -0500

    pull list of restaurants and display them on website.

--- vagrant/restaurant/webserver_hello.py
+++ vagrant/restaurant/webserver_hello.py
@@ -1,37 +1,41 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem
 
 class webserverHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
-		#create engine (connection to db)
-		engine = create_engine('sqlite:///restaurantmenu.db')
-		# Bind engine to Base class
-		Base.metadata.bind = engine
-		#bucket to store instructions to send to db
-		DBSession = sessionmaker(bind = engine)
-		# create instance of session
-		session = DBSession()
 		try:
-			self.send_response(200)
-			self.send_header('Content-type', 'text/html')
-			self.end_headers()
+			if self.path.endswith("/hello"):
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
 				
-			output = ""
-			output += "<html><body>"
-			restaurants = session.query(Restaurant).all()
-			for restaurant in restaurants:
-				rname = restaurant.name
-				print rname
-				output += "<h1> %s </h1>" % restaurant.name
-				output += "<p>Edit</p>"
-				output += "<p>Delete</p>"
-			output += "</body></html>"
-			self.wfile.write(output)
-			print output
-			return
+				output = ""
+				output += "<html><body>"
+				output += "Hello!"
+				output += """<form method='POST' enctype='multipart/form-data' 
+					action='/hello'><h2>What would you like me to say?</h2>
+					<input name='message' type='text' ><input type='submit'
+					value='Submit'></form>"""
+				output += "</body></html>"
+				self.wfile.write(output)
+				print output
+				return
+			if self.path.endswith("/hola"):
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+				
+				output = ""
+				output += "<html><body>"
+				output += "&#161Hola! <a href = '/hello'>Back to Hello</a>"
+				output += """<form method='POST' enctype='multipart/form-data' 
+					action='/hello'><h2>What would you like me to say?</h2>
+					<input name='message' type='text' ><input type='submit'
+					value='Submit'></form>"""
+				output += "</body></html>"
+				self.wfile.write(output)
+				print output
+				return
 				
 		except IOError:
 			self.send_error(404, "File Not Found %s" % self.path)
@@ -67,8 +71,6 @@ def main():
 		server = HTTPServer(('',port), webserverHandler)
 		print "Web server running on port %s" % port
 		server.serve_forever()
-		
-		
 		
 	except KeyboardInterrupt:
 		print "^C entered, stopping web server..."
